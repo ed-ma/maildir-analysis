15f384eee999b7032e37cd2ba9395e8ff93990a6
Eduard Mann <edma@umija.org>
2018-08-20 13:12:21 +0200

    Error handling improved.

--- count_gmail.py
+++ count_gmail.py
@@ -6,6 +6,7 @@ import email
 import time
 import sys
 import os.path
+import logging
 
 md_name = os.path.expanduser(sys.argv[1])
 
@@ -26,10 +27,12 @@ for intermsg in inbox.items():
     date = msg.get("Date")
 
     if date == None:
+        logging.warning('No Date! Key: %s', intermsg[0])  # will print a message to the console
         continue
     else:
         date = email.utils.parsedate(date)
         if date == None:
+            logging.warning('No Date, after utils parsing! Key: %s', intermsg[0])  # will print a message to the console
             continue
         else:
             date = time.mktime(date)
@@ -40,20 +43,24 @@ for intermsg in inbox.items():
     sender= cls_mail(msg.get("From"))
     to = cls_mail(msg.get("To"))
 
-    if len(sender.split("@")) >= 1:
+    try:
       sender_domain = '.'.join(sender.split("@")[1].split(".")[-2:])
-    else:
+    except:
       sender_domain = "NA"
+      logging.warning('Split for Sender doomain failed. Key: %s', intermsg[0])  # will print a message to the console
 
-    if len(to.split("@")) >= 1:
+    try:
       to_domain = '.'.join(to.split("@")[1].split(".")[-2:])
-    else:
+    except:
       to_domain = "NA"
+      logging.warning('Split for To doomain failed. Key: %s', intermsg[0])  # will print a message to the console
 
-    if intermsg[0].split(",") >= 1:
+    try:
       msg_size = intermsg[0].split(",")[1].split("=")[1]
-    else:
+    except:
       msg_size = "NA"
+      logging.warning('Split for message size failed. Key: %s', intermsg[0])  # will print a message to the console
+
 
     # put it together and output
-    print("\t".join([str(date), to, sender, sender_domain, msg_size]))
+    print("\t".join([str(date), to, sender, to_domain, sender_domain, msg_size]))
